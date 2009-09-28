9e3f771f74685f9d92d2185c2593d3e4056983b7
Niclas Axelsson <niclas@burbas.se>
2009-09-28 16:07:42 +0000

    A very basic logger-module
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@1 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

---
+++ logger/logger.erl
@@ -0,0 +1,30 @@
+-module(logger).
+-export([start/1]).
+
+
+% A very simple logger (External)
+
+% Starts the server and opens a file named "Filename" and calls logger_loop-function
+start(Filename) ->
+	{ok, IoDevice} = file:open(Filename, [raw,append]),
+	file:write(IoDevice, "Starting logger for JobID: "++Filename++"\n"),
+	logger_loop(IoDevice).
+
+
+% Writes a "Stopping"-message to the file and then closes it.
+stop(IoDevice) ->
+	file:write(IoDevice, "Stopping logger\n"),
+	file:close(IoDevice).
+
+% Main loop. Waits for messages on form:
+	% {event, PID, Message}
+% and prints it to a filehandler "IoDevice".
+logger_loop(IoDevice) ->
+	receive 
+		{event, PID, Message} -> 
+			Time = dh_date:format("H:i:s", erlang:localtime()),
+			file:write(IoDevice, ("[PID] "++Time++" - "++Message++"\n")),
+			logger_loop(IoDevice);
+		{stop} ->
+			stop(IoDevice)
+	end.

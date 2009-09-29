d92d8d3b3e7e43f25679a3a8ba07ea4e947aa48e
Fredrik Andersson <sedrik@consbox.se>
2009-09-29 12:45:17 +0000

    Made the supervisor print helloworld =)
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@128 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/clientSupervisor.erl
+++ src/clientSupervisor.erl
@@ -2,29 +2,24 @@
 %%% Description : The client supervisor
 %%% Created : Tue Sep 29 08:58:17 CEST 2009
 
+%-vsn('$Rev$').
+
 -module(clientSupervisor).
 -behaviour(supervisor).
-
--define(HEARTBEAT, heartbeat).
 
 -export([start_link/0]).
 -export([init/1, printer/1]).
 
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, no_args).
+    supervisor:start_link(?MODULE, no_args).
 
 init(no_args) ->
     {ok,{{one_for_one, 1, 60},
             [{printer, {?MODULE, printer, [no_args]},
-                 permanent, brutal_kill, worker, [clientSupervisor]},
-             {?HEARTBEAT, {?HEARTBEAT, init, [whereis(printer)]},
-                 permanent, brutal_kill, worker, [?HEARTBEAT]}]
+                 permanent, brutal_kill, worker, [clientSupervisor]}]
         }}.
 
 printer(no_args) ->
-    io:format("Printer: Hello world"),
-    receive
-        Anything ->
-            io:print("Got message: ~p ~n", [Anything]),
-            printer(no_args)
-    end.
+    io:format("Printer: Hello world ~n"),
+    timer:sleep(1000),
+    printer(no_args).

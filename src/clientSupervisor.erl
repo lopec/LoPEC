45ba7b34c67758473e9e86efa035570eac33b4b6
Fredrik Andersson <sedrik@consbox.se>
2009-09-29 14:25:07 +0000

    Finished documenting for now
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@135 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/clientSupervisor.erl
+++ src/clientSupervisor.erl
@@ -1,22 +1,67 @@
-%%% Author  : Sedrik
-%%% Description : The client supervisor
-%%% Created : Tue Sep 29 08:58:17 CEST 2009
-
-%-vsn('$Rev$').
-
+%%%-------------------------------------------------------------------
+%%% @author Fredrik Andersson <sedrik@consbox.se>
+%%% @copyright (C) 2009, Clusterbusters
+%%% @doc The client supervisor
+%%% Supervises the client supervision tree
+%%% @end
+%%% Created : 29 Sep 2009 by Fredrik Andersson <sedrik@consbox.se>
+%%%-------------------------------------------------------------------
 -module(clientSupervisor).
 -behaviour(supervisor).
 
+%% API
 -export([start_link/0]).
+
+%% Supervisor callbacks
 -export([init/1]).
 
-start_link() ->
-    supervisor:start_link(?MODULE, no_args).
+%%%===================================================================
+%%% API functions
+%%%===================================================================
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, no_args).
+
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%%
+%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
+%%                     ignore |
+%%                     {error, Reason}
+%% @end
+%%--------------------------------------------------------------------
 init(no_args) ->
     {ok,{{one_for_one, 1, 60},
-            [child(heartbeat, self())]}}.
+            [ child(heartbeat, self())]}}.
 
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Simple helper function to make the child specefication list easier
+%% to read.
+%%
+%% @spec child(Module, Args) -> {ChildSpec}
+%% @end
+%%--------------------------------------------------------------------
 child(Module, no_args) ->
     {Module, {Module, start_link, []}, permanent, brutal_kill, worker, [Module]};
 child(Module, Args) ->

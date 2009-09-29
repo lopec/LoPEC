efeff06bece2287067c9718986feecdd1f4f7b79
Fredrik Andersson <sedrik@consbox.se>
2009-09-29 09:15:28 +0000

    Work in progess
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@120 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/clientSupervisor.erl
+++ src/clientSupervisor.erl
@@ -1,8 +1,26 @@
 %%% Author  : Sedrik
 %%% Description : The client supervisor
-%%% Created :
+%%% Created : Tue Sep 29 08:58:17 CEST 2009
 
 -module(clientSupervisor).
--export([]).
+-behaviour(supervisor).
 
-spawn() -> {error, implementationNeeded}.
+-export([start_link/0]).
+-export([init/1, printer/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, no_args).
+
+init(no_args) ->
+    {ok,{{one_for_one, 1, 60},
+            [{printer, {?MODULE, printer, [no_args]},
+                 permanent, brutal_kill, worker, [clientSupervisor]},
+             {heartbeat, {heartbeat, init, [printer]},
+                 permanent, brutal_kill, worker, [heartbeat]}]
+        }}.
+
+printer(no_args) ->
+    receive
+        Anything ->
+            io:print("Got message: ~p ~n", [Anything])
+    end.

073a615e8cb06f360773526e4b9b00c98c768ee0
Niclas Axelsson <niclas@burbas.se>
2009-09-29 11:25:06 +0000

    testing svn-hooks
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@13 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

---
+++ src/logger/svn-hook-test.erl
@@ -0,0 +1,41 @@
+-module(logger_ext).
+-export([start/1]).
+-vsn('$Rev$').
+ 
+% A very simple logger (External)
+% ====================
+% Rev: $rev$
+% Version: $LastChangedDate$
+% Maybe we should have different log-types (Error, Warning, Info)?
+
+
+% Starts the server and opens a file named "Filename" 
+% and calls logger_loop-function
+start(Filename) ->
+	{ok, IoDevice} = file:open(Filename, [raw,append]),
+	file:write(IoDevice, "Starting logger for JobID: "++Filename++"\n"),
+	logger_loop(IoDevice).
+
+
+% Writes a "Stopping"-message to the file and then closes it.
+stop(IoDevice) ->
+	file:write(IoDevice, "Stopping logger\n"),
+	file:close(IoDevice).
+
+% Main loop. Waits for messages on form:
+	% {event, PID, Message}
+% and prints it to a filehandler "IoDevice".
+logger_loop(IoDevice) ->
+	receive 
+		{event, PID, Message} -> 
+			Time = dh_date:format("H:i:s", erlang:localtime()),
+			file:write(IoDevice, ("[PID] "++Time++" - "++Message++"\n")),
+			logger_loop(IoDevice);
+        {event, PID, Type, Message} ->
+            Time = dh_date:format("H:i:s", erlang:localtime()),
+            file:write(IoDevice, ("[PID] ["++Type++"] "++Time++" - "
+                        ++Message++"\n")),
+            logger_loop(IoDevice);
+		{stop} ->
+			stop(IoDevice)
+	end.

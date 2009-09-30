09ff627a68e1f37cceac19622a4331ea20aaedb6
Fredrik Andersson <sedrik@consbox.se>
2009-09-30 14:37:57 +0000

    Documented clientApp
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@145 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/clientApp.erl
+++ src/clientApp.erl
@@ -1,10 +1,45 @@
+%%%-------------------------------------------------------------------
+%%% @author Fredrik Andersson <sedrik@consbox.se>
+%%% @copyright (C) 2009, Clusterbusters
+%%% @doc The client application module
+%%% Handles starting and stoping of the client
+%%% @end
+%%% Created : 29 Sep 2009 by Fredrik Andersson <sedrik@consbox.se>
+%%%-------------------------------------------------------------------
 -module(clientApp).
 -behaviour(application).
 
+%% Application callbacks
 -export([start/2, stop/1]).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application is started using
+%% application:start/[1,2], and should start the processes of the
+%% application. If the application is structured according to the OTP
+%% design principles as a supervision tree, this means starting the
+%% top supervisor of the tree.
+%%
+%% @spec start(StartType, StartArgs) -> {ok, Pid} |
+%%                                      {ok, Pid, State} |
+%%                                      {error, Reason}
+%%      StartType = normal | {takeover, Node} | {failover, Node}
+%%      StartArgs = term()
+%% @end
+%%--------------------------------------------------------------------
 start(_Type, _Args) ->
     clientSupervisor:start_link().
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called whenever an application has stopped. It
+%% is intended to be the opposite of Module:start/2 and should do
+%% any necessary cleaning up. The return value is ignored.
+%%
+%% @spec stop(State) -> void()
+%% @end
+%%--------------------------------------------------------------------
 stop(_State) ->
     ok.

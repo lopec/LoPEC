c155d74e745b12366101f139386acfcd65edc546
Vasilij Savin <vasilij.savin@gmail.com>
2009-10-02 09:27:47 +0000

    Added test description.
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@50 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/ecg_test.sh
+++ src/ecg_test.sh
@@ -1,3 +1,13 @@
+## Test description:
+## 1. Start a node
+## 2. Start a server
+## 2.1. Test suite will send {new_node} message
+## 2.2. {nodeup} msg should be generated and logged
+## 3. Kill a node
+## 3.1. {nodedown} should be generated
+## 4. Send garbage message - should be logged fully
+## 5. Cleanup
+
 # Starting server
 erl -sname server -setcookie busters -run ecg_tests ecg_test -detached
 

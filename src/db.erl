509741bb4f64aa19ad0a221ef4ae5c82af7d36f5
Henrik Thalin <henkethalin@hotmail.com>
2009-10-05 14:30:21 +0000

    Fixed some db issues and functionality
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@60 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/db.erl
+++ src/db.erl
@@ -19,7 +19,7 @@
 -define(SERVER, db_server).
 
 %% APIs for management of the databases
--export([start/0, start_link/0, stop/0, init_db/0, create_tables/0]).
+-export([start/0, start_link/0, stop/0, create_tables/0]).
 
 %% APIs for external access to the job table
 -export([add_job/4, remove_job/1, 
@@ -35,119 +35,264 @@
 	 get_task_callback_path/1, get_task_input_path/1,
 	 get_task_state/1, get_task_priority/1, get_task_type/1, 
 	 set_task_state/2, set_task_priority/2,
-	 list_tasks/0, list_tasks/1]).
+	 list_tasks/0, list_tasks/1, list_node_tasks/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
 	 terminate/2, code_change/3]).
 
-
 %%--------------------------------------------------------------------
-%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
-%% Description: Starts the server
+%% @private
+%% @doc
+%%
+%% Starts the database gen_server.
+%%
+%% @spec start()
+%% @end
 %%--------------------------------------------------------------------
 start() ->
     start_link().
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Starts the database gen_server.
+%%
+%% @spec start_link()
+%% @end
+%%--------------------------------------------------------------------
 start_link() ->
-    gen_server:start_link({global, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Stops the database gen_server.
+%%
+%% @spec stop()
+%% @end
+%%--------------------------------------------------------------------
 stop() ->
-    gen_server:cast({global, ?SERVER}, stop).
+    gen_server:cast(?SERVER, stop).
 
-init_db() ->
-    gen_server:call({global, ?SERVER}, init_db).
-
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Creates the tables used for keeping track of the jobs and
+%% tasks.
+%%
+%% @spec create_tables()
+%% @end
+%%--------------------------------------------------------------------
 create_tables() ->
-    gen_server:call({global, ?SERVER}, create_tables).
+    gen_server:call(?SERVER, create_tables).
 
 
 %%====================================================================
 %% JOB TABLE APIs
 %%====================================================================
-
 %%--------------------------------------------------------------------
-%% Function: add_job() -> JobId
-%% Description: Adds a job to the job list.
+%% @private
+%% @doc
+%%
+%% Adds a job to the job table.
+%% 
+%% @spec add_job() -> ok
+%% @end
 %%--------------------------------------------------------------------
-
-
 add_job(CallbackPath, InputPath, ReplyId, Priority) ->
-    gen_server:call({global, ?SERVER}, {add_job, CallbackPath, InputPath,
+    gen_server:call(?SERVER, {add_job, CallbackPath, InputPath,
 					ReplyId, Priority}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Removes a job from the job table.
+%% 
+%% @spec remove_job() -> ok
+%% @end
+%%--------------------------------------------------------------------
 remove_job(JobId) ->
-    gen_server:call({global, ?SERVER}, {remove_job, JobId}).
+    gen_server:call(?SERVER, {remove_job, JobId}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns a job from the job table which has its current status set
+%% to 'available'.
+%% 
+%% @spec get_job() -> Record
+%% @end
+%%--------------------------------------------------------------------
 get_job() ->
-    gen_server:call({global, ?SERVER}, {get_job}).
+    gen_server:call(?SERVER, {get_job}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the full record of a job given its 'JobId'.
+%% 
+%% @spec get_job_info(JobId) -> Record
+%% @end
+%%--------------------------------------------------------------------
 get_job_info(JobId) ->
-    gen_server:call({global, ?SERVER}, {get_job_info, JobId}).
+    gen_server:call(?SERVER, {get_job_info, JobId}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Changes the state of the given job with id 'JobId' to 'State'.
+%% 
+%% @spec set_job_state() -> ok
+%% @end
+%%--------------------------------------------------------------------
 set_job_state(JobId, State) ->
-    gen_server:call({global, ?SERVER}, {set_job_state, JobId, State}).
+    gen_server:call(?SERVER, {set_job_state, JobId, State}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Changes the progress of the given job with id 'JobId' to 'Progress'.
+%% 
+%% @spec set_job_progress() -> ok
+%% @end
+%%--------------------------------------------------------------------
 set_job_progress(JobId, Progress) ->
-    gen_server:call({global, ?SERVER}, {set_job_progress, JobId, Progress}).
+    gen_server:call(?SERVER, {set_job_progress, JobId, Progress}).
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Changes the priority of the given job with id 'JobId' to 'Priority'.
+%% 
+%% @spec set_job_priority() -> ok
+%% @end
+%%--------------------------------------------------------------------
 set_job_priority(JobId, Priority) ->
-    gen_server:call({global, ?SERVER}, {set_job_priority, JobId, Priority}).
- 
+    gen_server:call(?SERVER, {set_job_priority, JobId, Priority}).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the state of the given job with id 'JobId'.
+%% 
+%% @spec get_job_state() -> State
+%% @end
+%%--------------------------------------------------------------------
 get_job_state(JobId) ->
     Job = get_job_info(JobId),
     Job#job.current_state.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the progress of the given job with id 'JobId'.
+%% 
+%% @spec get_job_progress() -> Progress
+%% @end
+%%--------------------------------------------------------------------
 get_job_progress(JobId) ->
     Job = get_job_info(JobId),
     Job#job.current_progress.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the callback path of the given job with id 'JobId'.
+%% 
+%% @spec get_callback_path() -> CallbackPath
+%% @end
+%%--------------------------------------------------------------------
 get_job_callback_path(JobId) ->
     Job = get_job_info(JobId),
     Job#job.callback_path.    
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the input path of the job with id 'JobId'.
+%% 
+%% @spec get_job_input_path() -> InputPath
+%% @end
+%%--------------------------------------------------------------------
 get_job_input_path(JobId) ->
     Job = get_job_info(JobId),
     Job#job.input_path.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the reply id of the job with id 'JobId'.
+%% 
+%% @spec get_job_reply_id() -> ReplyId
+%% @end
+%%--------------------------------------------------------------------
 get_job_reply_id(JobId) ->
     Job = get_job_info(JobId),
     Job#job.reply_id.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns the priority of the job with id 'JobId'.
+%% 
+%% @spec get_job_priority() -> Priority
+%% @end
+%%--------------------------------------------------------------------
 get_job_priority(JobId) ->
     Job = get_job_info(JobId),
     Job#job.priority.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% Returns a list containing all id's of the jobs in the job table.
+%% 
+%% @spec list_jobs() -> ListOfJobIds
+%% @end
+%%--------------------------------------------------------------------
 list_jobs() ->
-    F = fun() ->
-		mnesia:all_keys(job)
-	end,
-    {atomic, Result} = mnesia:transaction(F),
-    Result.
+    gen_server:call(?SERVER, {list_jobs}).
 
 %%====================================================================
 %% TASK TABLE APIs
 %%====================================================================
 
 add_task(JobId, TaskType, CallbackPath, InputPath, Priority) ->
-    gen_server:call({global, ?SERVER}, {add_task, JobId, 
+    gen_server:call(?SERVER, {add_task, JobId, 
 					TaskType, CallbackPath, 
 					InputPath, available, Priority}).
 
 remove_task(TaskId) ->
-    gen_server:call({global, ?SERVER}, {remove_task, TaskId}).
+    gen_server:call(?SERVER, {remove_task, TaskId}).
 
 get_task() ->
-    gen_server:call({global, ?SERVER}, {get_task}).
+    gen_server:call(?SERVER, {get_task}).
 
 get_task_info(TaskId) ->
-    gen_server:call({global, ?SERVER}, {get_task_info, TaskId}).
+    gen_server:call(?SERVER, {get_task_info, TaskId}).
 
 set_task_state(TaskId, State) ->
-    gen_server:call({global, ?SERVER}, {set_task_state, TaskId, State}).
+    gen_server:call(?SERVER, {set_task_state, TaskId, State}).
 
 set_task_priority(TaskId, Priority) ->
-    gen_server:call({global, ?SERVER}, {set_task_priority, TaskId, Priority}).
+    gen_server:call(?SERVER, {set_task_priority, TaskId, Priority}).
  
 get_task_state(TaskId) ->
     Task = get_task_info(TaskId),
@@ -170,16 +315,13 @@ get_task_priority(TaskId) ->
     Task#task.priority.
 
 list_tasks() ->
-    gen_server:call({global, ?SERVER}, {list_tasks}).
+    gen_server:call(?SERVER, {list_tasks}).
 
 list_tasks(JobId) ->
-    gen_server:call({global, ?SERVER}, {list_tasks, JobId}).
+    gen_server:call(?SERVER, {list_tasks, JobId}).
 
-%% list_tasks(JobId) ->
-%%      ok.
-
-%% list_node_tasks(NodeId) ->
-%%      ok.
+list_node_tasks(NodeId) ->
+    gen_server:call(?SERVER, {list_node_tasks, NodeId}).
 		
 %%====================================================================
 %% gen_server callbacks
@@ -193,8 +335,8 @@ list_tasks(JobId) ->
 %% Description: Initiates the server
 %%--------------------------------------------------------------------
 init(_Args) ->
-    NodeList = [node()|nodes()],
-    mnesia:create_schema([node()|nodes()]),
+    NodeList = [node()],             %TODO: add distribution to other
+    mnesia:create_schema([node()]),  %database nodes!
     application:start(mnesia),
     {ok, NodeList}.
 
@@ -242,16 +384,16 @@ handle_call({get_job}, _From, State) ->
 				 reply_id = '_',
 				 priority = '_'},
 		Result = '$1',
-	        mnesia:select(job, [{MatchHead, [], [Result]}])
+	        mnesia:select(job, [{MatchHead, [], [Result]}], 1, read)
 	end,
 
-    {atomic, Result} = mnesia:transaction(F),
+    Result = mnesia:transaction(F),
     case Result of
-	[Job|_Jobs] ->
-	    {reply, Job, State};
-	[] ->
-	    {reply, no_job, State}
-    end;
+	{atomic, {[Job], _Cont}} ->
+ 	    {reply, get_job_info_on_server(Job), State};
+ 	{atomic, '$end_of_table'} ->
+ 	    {reply, no_job, State}
+     end;
 
 handle_call({get_job_info, JobId}, _From, State) ->
     Job = get_job_info_on_server(JobId),
@@ -330,13 +472,13 @@ handle_call({get_task}, _From, State) ->
  				  current_state = available,
  				  priority = '_'},
  		Result = '$1',
- 	        mnesia:select(task, [{MatchHead, [], [Result]}])
+ 	        mnesia:select(task, [{MatchHead, [], [Result]}], 1, read)
  	end,
-    {atomic, Result} = mnesia:transaction(F),
+    Result = mnesia:transaction(F),
     case Result of
-	[Head|_Tasks] ->
-	    Task = get_task_info_on_server(Head),
-	    remove_task_on_server(Head),
+	{atomic, {[First], _Cont}} ->
+	    Task = get_task_info_on_server(First),
+	    remove_task_on_server(First),
 	    add_task_on_server(Task#task.task_id, 
 			       Task#task.job_id, 
 			       Task#task.task_type,
@@ -344,8 +486,8 @@ handle_call({get_task}, _From, State) ->
 			       Task#task.input_path, 
 			       reserved,
 			       Task#task.priority),
- 	    {reply, Head, State};
- 	[] ->
+ 	    {reply, Task, State};
+ 	{atomic, '$end_of_table'} ->
  	    {reply, no_task, State}
     end;
 
@@ -390,22 +532,34 @@ handle_call({list_tasks}, _From, State) ->
     {atomic, Result} = mnesia:transaction(F),
     {reply, Result, State};
 
-handle_call({list_tasks, _JobId}, _From, State) ->
-%%     F = fun() ->		
-%% 		MatchHead = #task{task_id = '_',
-%% 				  job_id = '$1',
-%% 				  task_type = '_',
-%% 				  callback_path = '_',
-%% 				  input_path = '_',
-%% 				  current_state = '_',
-%% 				  priority = '_'},
-%% 		Result = '$1',
-%% 		Guard = {'=', '$1', JobId},
-%% 	        mnesia:select(job, [{MatchHead, [Guard], [Result]}])		
-%% 	end,
-%%     {atomic, Result} = mnesia:transaction(F),
-%%     {reply, Result, State}.
-    {reply, [], State}.
+handle_call({list_tasks, JobId}, _From, State) ->
+    F = fun() ->
+		MatchHead = #task_job{task_id = '$1',
+				      job_id = '$2'},
+ 		Result = '$1',
+		Guard = {'==', '$2', JobId},
+ 	        mnesia:select(task_job, [{MatchHead, [Guard], [Result]}])
+ 	end,
+    {atomic, Result} = mnesia:transaction(F),
+    {reply, Result, State};
+handle_call({list_node_tasks, NodeId}, _From, State) ->
+    F = fun() ->
+		MatchHead = #task{task_id = '$1',
+ 				  job_id = '_',
+ 				  task_type = '_',
+ 				  callback_path = '_',
+ 				  input_path = '_',
+ 				  current_state = {'$2', '$3'},
+ 				  priority = '_'},
+ 		Result = '$1',
+		Guard1 = {'==', '$2', reserved},
+		Guard2 = {'==', '$2', assigned},
+		Guard3 = {'==', '$3', NodeId},
+ 	        mnesia:select(task, [{MatchHead, [Guard1, Guard2, Guard3], 
+				      [Result]}], 1, read)
+ 	end,
+    Result = mnesia:transaction(F),
+    {reply, Result, State}.
 
 %%--------------------------------------------------------------------
 %% Function: handle_cast(Msg, State) -> {noreply, State} |
@@ -416,7 +570,7 @@ handle_call({list_tasks, _JobId}, _From, State) ->
 
 handle_cast(stop, State) ->
     application:stop(mnesia),
-    {noreply, State};
+    {stop, normal, State};
 
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -495,14 +649,17 @@ add_task_on_server(TaskId, JobId, TaskType, CallbackPath, InputPath,
 				   callback_path = CallbackPath,
 				   input_path = InputPath,
 				   current_state = CurrentState,
-				   priority = Priority})
+				   priority = Priority}),
+		mnesia:write(#task_job{task_id = TaskId,
+				   job_id = JobId})
 	end,
     mnesia:transaction(F),
     TaskId.
 
 remove_task_on_server(TaskId) ->    
     F = fun() ->
-		mnesia:delete({task, TaskId})
+		mnesia:delete({task, TaskId}),
+		mnesia:delete({task_job, TaskId})
 	end,
     mnesia:transaction(F),
     ok.

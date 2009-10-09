3269eb083810119c8734e6996d4079b4a849b414
Vasilij Savin <vasilij.savin@gmail.com>
2009-10-09 07:08:37 +0000

    Dispatcher. Revised, expanded and tested.
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@93 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- src/dispatcher.erl
+++ src/dispatcher.erl
@@ -1,28 +1,24 @@
 %%%-------------------------------------------------------------------
-%%% @author Axel <>
+%%% @author Axel, Vasilij Savin <>
 %%% @copyright (C) 2009, Axel
 %%% @doc
-%%% Receives job/task requests from a node, and returns the first
-%%% available job to it. If no jobs are found, the first available
-%%% task is instead returned. If there are no jobs or tasks, it
-%%% returns what db:get_task() returns in that case - the atom
-%%% no_task, presumably.
+%%% Receives task requests from a node, and returns the first
+%%% available task to it. If there is no available task, it
+%%% does not return and let request time out.
+%%% Also listens to reports from nodes and marks tasks as completed.
 %%% @end
 %%% Created : 30 Sep 2009 by Axel <>
 %%%-------------------------------------------------------------------
 -module(dispatcher).
-
 -behaviour(gen_server).
+-include("../include/db.hrl").
 
 %% API
--export([start_link/0, get_work/0]).
+-export([start_link/0, get_task/2,report_task_done/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3, find_job/1, find_task/1]).
-
--define(SERVER, dispatcher_server). 
-
+         terminate/2, code_change/3, find_task/2]). 
 
 %%%===================================================================
 %%% API
@@ -30,82 +26,119 @@
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Starts the server
+%% Starts the server.
 %%
 %% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
 %% @end
 %%--------------------------------------------------------------------
 start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %%--------------------------------------------------------------------
 %% @doc
-%% Returns the first available job, or if no jobs are available, the
-%% first available task, or if no tasks are available,
-%% no_task. Assuming db:get_task() returns no_task, and db:get_job()
-%% no_job (when applicable for both).
+%% Returns the first available task.
+%% If no tasks are available, just lets request to time out.
 %%
-%% @spec get_work() -> {ok, JobID} | {ok, TaskID} | no_task
 %% @end
 %%--------------------------------------------------------------------
-get_work() ->
-    gen_server:cast({get_job, self()}, waiting_for_requests).
+get_task(NodeId, PID) ->
+    gen_server:cast(?MODULE, {task_request, NodeId, PID}).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Marks the task as being completely done. The results should be
+%% posted on storage before calling this method.
+%% @end
+%%--------------------------------------------------------------------
+report_task_done(TaskId) ->
+    gen_server:call(?MODULE, {task_done, TaskId}).
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @private
 %% @doc
-%% Initiates the server
+%% Initiates the server. Currently no special setup is needed.
 %%
 %% @spec init(Args) -> {ok, State} 
 %% @end
 %%--------------------------------------------------------------------
 init([]) ->
-    {ok, waiting_for_requests}.
+    register (logger, spawn_link(logger_ext, start, ["test.logging"])),
+    {ok, []}.
 
 %%--------------------------------------------------------------------
-%% @private
 %% @doc
-%% Not implemented, not expecting any calls to this function.
+%%
+%% Expects task requests from nodes, and passes such requests to the
+%% find_task function.
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} 
+%% @end
+%%--------------------------------------------------------------------
+handle_cast({task_request, NodeId, From}, _State) ->
+    spawn(?MODULE, find_task, [From, NodeId]),
+    {noreply, []};
+handle_cast(Msg, State) ->
+    io:format("Wrong message received: ~w", [Msg]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @doc
 %%
 %% @spec handle_call(Request, From, State) ->
 %%                                   {reply, ok, State} 
 %% @end
 %%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
+handle_call({task_done, TaskId}, _From, State) ->
+    Status = db:get_task_state(TaskId),
+    logger ! {event, self(), 
+              io_lib:format("before done ~p is ~p!", [TaskId, Status])},
+    
+    db:mark_done(TaskId),
+    logger ! {event, self(), 
+              io_lib:format("after done ~p is ~p!", [TaskId, db:get_task_state(TaskId)])},
     Reply = ok,
     {reply, Reply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Expects job requests from nodes, and passes such requests to the
-%% two internal functions find_job/1 and possibly find_task/1.
-%%
-%% @spec handle_cast(Msg, State) -> ok | {noreply, State} 
-%% @end
-%%--------------------------------------------------------------------
-handle_cast({job_req, From}, waiting_for_requests) ->
-    spawn(?MODULE, find_job, [From]);
-handle_cast(_Msg, State) ->
-    {noreply, State}.
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @private
 %% @doc
-%% Not implemented, not expecting any calls to this function.
 %%
-%% @spec handle_info(Info, State) -> {noreply, State} 
+%% Sends a message to given PID with the first found task in the DB,
+%% and tells the ECG to register this new node with that PID. If no
+%% task is found, it terminates and lets request time out.
+%%
+%% @spec find_task(From) -> ok
 %% @end
 %%--------------------------------------------------------------------
-handle_info(_Info, State) -> %template default
-    {noreply, State}.
+find_task(RequesterPID, NodeId) ->
+    case db:get_task(NodeId) of
+        % If no task found - let the request time out and try again
+        % Therefore we just terminate
+        no_task -> 
+            ok;
+        Task ->
+            % Get ready with integration testing!
+            %global:send(ecg_server, {new_node, NodeId}),
+            RequesterPID ! {task_response, Task, self()},
+            receive
+                {task_accepted, TaskId} ->
+                    db:assign_task(TaskId, NodeId);
+                Msg ->
+                    io:format("Wrong message received: ~w", [Msg])
+            after 1000 ->
+                    db:remove_reservation(Task#task.task_id)
+            end
+    end.
 
+%%%===================================================================
+%%% Not implemented stuff
+%%%===================================================================
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -133,45 +166,13 @@ terminate(_Reason, _State) -> %template default
 code_change(_OldVsn, State, _Extra) -> %template default
     {ok, State}.
 
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
+%% Not implemented, not expecting any calls to this function.
 %%
-%% Sends a message to given PID with the first found job in the DB,
-%% and tells the ECG to register this new node with that PID.  If no
-%% job is found, it instead calls find_task/1 with the given PID.
-%%
-%% @spec find_job(From) -> ok
+%% @spec handle_info(Info, State) -> {noreply, State} 
 %% @end
 %%--------------------------------------------------------------------
-find_job(From) ->
-    case db:get_job() of
-        no_job -> find_task(From);
-        Job -> 
-            global:send(ecg_server, {new_node, From}),
-            From ! {new_job, Job}
-    end.
-
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% Sends a message to given PID with the first found task in the DB,
-%% and tells the ECG to register this new node with that PID. If no
-%% task is found, it instead sends {no_task_found}.
-%%
-%% @spec find_task(From) -> ok
-%% @end
-%%--------------------------------------------------------------------
-find_task(From) ->
-    case db:get_task() of
-        no_task -> From ! {no_task_found};
-        Task -> 
-            global:send(ecg_server, {new_node, From}),
-            From ! {new_task, Task}
-    end.
+handle_info(_Info, State) -> %template default
+    {noreply, State}.
--- src/dispatcher_tests.erl
+++ src/dispatcher_tests.erl
@@ -4,40 +4,71 @@
 %%% @doc
 %%%
 %%% Contains the unit tests for the dispatcher. As this requires a
-%%% table to be created and contain data we know exatly, these tests
+%%% table to be created and contain data we know exactly, these tests
 %%% must not be run while the system is live.
 %%%
 %%% @end
 %%% Created : 1 Oct 2009 by Axel <>
 %%%-------------------------------------------------------------------
 -module(dispatcher_tests).
-
+-include("../include/db.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
+init_per_test_case() ->    
+    db:create_tables().
 
-get_task_test() ->
+end_per_test_case() ->
+    db:delete_tables().
+
+init_test() ->
     db:start(),
-    db:create_tables(),
-    TestJob = somestuff,
-    db:add_job({TestJob}),
-    TestJob = dispatcher:get_work(),
-    TestTask = otherstuff,
-    db:add_job({TestJob}),
-    db:add_task(TestTask),
-    TestItem = dispatcher:get_work(),
+    dispatcher:start_link().
 
-    case TestItem of
-        TestJob ->
-            TestItem2 = dispatcher:get_work(),
-            case TestItem2 of
-                TestTask ->
-                    ok;
-                Other ->
-                    exit("Was expecting TestTask after getting TestJob,"
-                         "got ~p~n", [Other])
-            end;
-        TestTask ->
-            exit("Was expecting TestJob; got TestTask.");
-        Other ->
-            exit("Was expecting TestJob; got ~p~n", [Other])
-    end.
+task_allocation_test() ->
+    init_per_test_case(),
+    FreeTask1 = {1, map, "Callback",  "Input3", 0},
+    FId = db:add_task(FreeTask1),
+    dispatcher:get_task(node(), self()),
+    receive
+        {requestACK, Task, FinderPID} ->
+            io:format("Task received: ~w", [Task]),
+            ?assert(Task#task.task_id =:= FId),
+            ?assert(db:get_task_state(FId) =:= reserved),
+            FinderPID ! {task_accepted, Task#task.task_id, node(), self()},
+            ?assert(db:get_task_state(FId) =:= assigned);
+        Msg ->
+                io:format("Wrong message received: ~w", [Msg])            
+    end,
+    end_per_test_case().
+
+timeout_test() ->
+    init_per_test_case(),
+    dispatcher:get_task(node(), self()),
+    receive
+        Msg ->
+            io:format("Unexpected message received: ~w", [Msg])
+        after 1000 ->
+            ok
+    end,
+    end_per_test_case().
+
+task_completed_test() ->
+    init_per_test_case(),
+    
+    AssignedTask = {1, map, "Callback",  "Input6", 0},
+    AId = db:add_task(AssignedTask),
+    db:assign_task(AId, node()),
+    ?assertEqual(assigned, db:get_task_state(AId)),
+    dispatcher:report_task_done(AId),
+    logger ! {event, self(), 
+              io_lib:format("Before ~p is ~p!", [AId, db:get_task_state(AId)])},
+    Status = db:get_task_state(AId),
+    logger ! {event, self(), 
+              io_lib:format("After ~p is ~p!", [AId, db:get_task_state(AId)])},
+    ?assertEqual(done, Status),
+    
+
+    end_per_test_case().
+    
+end_test() ->
+    db:stop().

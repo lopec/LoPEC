9d016139775deb8b46d4c4d913f72263fffe80e6
Niclas Axelsson <niclas@burbas.se>
2009-10-12 15:09:26 +0000

    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@193 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- old_slave/src/computingProcess_tests.erl
+++
@@ -1,9 +0,0 @@
-%%% @private
--module(computingProcess_tests).
--include_lib("eunit/include/eunit.hrl").
-
-
-wrapper_starting_c_port_test_() ->
-    ?_assertMatch({ok, _Pid},computingProcess:start_link("tests/port_test",
-						      "map","a","asdf")),
-    ?_assertMatch(ok,computingProcess:stop()).

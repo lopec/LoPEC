48db33db9bcda6b1bd83702e40d0e685322c5a18
Gustav Simonsson <gusi7871@student.uu.se>
2009-10-13 13:07:10 +0000

    typo in the Makefile of logger, and a ecg_tests.erl that passes tests
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@227 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- lib/ecg/test/ecg_tests.erl
+++ lib/ecg/test/ecg_tests.erl
@@ -5,22 +5,24 @@
 -include_lib("eunit/include/eunit.hrl").
 
 run_test() ->
-    ok.
-%%    {setup,
-%%    fun () -> application:start(ecg) end,
-%%     fun (_) -> ok end,
-%%     fun (_) -> testing_ecg() end}.
+    ok,
+    {setup,
+     fun () -> application:start(ecg) end,
+     fun (_) -> ok end,
+     fun (_) -> testing_ecg() end}.
 
-%% TODO: Add description of test_update_list/function_arity
-%%testing_ecg() ->
-%%    Nodes = [compnode1, compnode2, compnode666],
+ %TODO: Add description of test_update_list/function_arity
+testing_ecg() ->
+    Nodes = [compnode1, compnode2, compnode666],
 
-%%     error_logger:info_msg("Starting: ~p~n"
-%%                           "They should all come up and die.~n",
-%%                           [Nodes]),
-%%     [slave:start_link("localhost", CompNode) || CompNode <- Nodes],
+     error_logger:info_msg("Starting: ~p~n"
+                           "They should all come up and die.~n",
+                           [Nodes]),
+     [slave:start_link("localhost", CompNode) || CompNode <- Nodes],
 
-%%     ecg_server:accept_message({badMsg, [b, s,s]}),
-%%     ecg_server:accept_message({stuff, [w, s,ss]}),
+     ecg_server:accept_message({badMsg, [b, s,s]}),
+     ecg_server:accept_message({stuff, [w, s,ss]}),
 
-%%     [slave:stop(CompNode) || CompNode <- Nodes].
+     [slave:stop(CompNode) || CompNode <- Nodes].
+
+

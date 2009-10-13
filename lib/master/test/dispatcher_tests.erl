3bf3a039504ab33cfa5398fc41d55e1eb2018f77
Vasilij Savin <vasilij.savin@gmail.com>
2009-10-13 11:45:02 +0000

    Most code is back.
    Working on remaining issue.
    
    
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@220 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- lib/master/test/dispatcher_tests.erl
+++ lib/master/test/dispatcher_tests.erl
@@ -24,14 +24,16 @@ init_test() ->
 
 task_allocation_test() ->
     init_per_test_case(),
-    TaskSpec = {255, split, "", 0},
-    io:format("Task created: ~p", [TaskSpec]),
-%%     FId =  dispatcher:create_task(TaskSpec),
-%%     io:format("Task received: ~p", [FId]),
+    JobId = db:add_job({raytracer, 0}),
+            io:format("Task received: ~w", [JobId]),
+    TaskSpec = {JobId, split, "", 0},
+            io:format("Task received: ~w", [TaskSpec]),
+    FId =  dispatcher:create_task(TaskSpec),
+            io:format("Task received: ~w", [FId]),
 %%     dispatcher:get_task(node(), self()),
 %%     receive
 %%         {task_response, Task} ->
-%%             io:format("Task received: ~p", [Task]),
+%%             io:format("Task received: ~w", [Task]),
 %%             ?assert(Task#task_tmp.task_id =:= FId),
 %%             ?assertEqual(split, Task#task_tmp.job_type),
 %%             ?assertEqual(255, Task#task_tmp.job_id);
@@ -42,34 +44,34 @@ task_allocation_test() ->
 
 %% This test case is used to verify that request times out
 %% when there is no free tasks, so taskFetcher can try again at later time.
-%% timeout_test() ->
-%%     init_per_test_case(),
-%%     dispatcher:get_task(node(), self()),
-%%     receive
-%%         Msg ->
-%%             io:format("Unexpected message received: ~p", [Msg])
-%%         after 1000 ->
-%%             ok
-%%     end,
-%%     end_per_test_case().
-%% 
-%% task_completed_test() ->
-%%     init_per_test_case(),
-%%     AssignedTask = {new_task, 1, map, 0},
-%%     AId = db:add_task(AssignedTask),
-%%     db:assign_task(AId, node()),
-%%     ?assertEqual(assigned, db:get_task_state(AId)),
-%%     dispatcher:report_task_done(AId),
-%%     Status = db:get_task_state(AId),
-%%     ?assertEqual(done, Status),
-%%     end_per_test_case().
-%%     
-%% create_job_test() ->
-%%     init_per_test_case(),
-%%     JobId = db:add_job({raytracer, 0}),
-%%     io:format("JobId: ~p", [JobId]),
-%%     end_per_test_case().
-%% 
+timeout_test() ->
+    init_per_test_case(),
+    dispatcher:get_task(node(), self()),
+    receive
+        Msg ->
+            io:format("Unexpected message received: ~w", [Msg])
+        after 1000 ->
+            ok
+    end,
+    end_per_test_case().
+
+task_completed_test() ->
+    init_per_test_case(),
+    AssignedTask = {new_task, 1, map, 0},
+    AId = db:add_task(AssignedTask),
+    db:assign_task(AId, node()),
+    ?assertEqual(assigned, db:get_task_state(AId)),
+    dispatcher:report_task_done(AId),
+    Status = db:get_task_state(AId),
+    ?assertEqual(done, Status),
+    end_per_test_case().
+    
+create_job_test() ->
+    init_per_test_case(),
+    JobId = db:add_job({raytracer, 0}),
+    io:format("JobId: ~w", [JobId]),
+    end_per_test_case().
+
 end_test() ->
     db:stop(),
     dispatcher:terminate(finished, []).

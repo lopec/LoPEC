4e35452cc4cba9be5fd0fd14ced2455d6176fde2
Vasilij Savin <vasilij.savin@gmail.com>
2009-10-14 12:40:01 +0000

    Fixed ugly bug.
    Now there should be input.file in storage define in config before testing.
    
    
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@278 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- lib/master/test/listener_tests.erl
+++ lib/master/test/listener_tests.erl
@@ -32,28 +32,27 @@ init_test() ->
 job_creation_test() ->
     init_per_test_case(),
     %testing if job is created properly
-    JobId = listener:new_job(raytracer, 
-                             "/home/chabbrik/Desktop/ClusterBusters/Busters/input.file"),
+    {ok, Root} = 
+        configparser:read_config("/etc/clusterbusters.conf", cluster_root),
+    
+    InputFile = Root ++ "input.file",
+    JobId = listener:new_job(raytracer, InputFile),
     Job = db:get_job_info(JobId),
     chronicler:info(Job),
     ?assertEqual(JobId, Job#job.job_id),
     ?assertEqual(raytracer, Job#job.job_type),
 
-    {ok, Root} = 
-        configparser:read_config("/etc/clusterbusters.conf", cluster_root),
     
     %Testing if task was properly created
     JobIdString = lists:flatten(io_lib:format("~p", [JobId])),
     TaskList = db:list_tasks(JobId),
-    ?assertMatch([A], TaskList),
+    ?assertMatch([_A], TaskList),
     Task = db:get_task_info(hd(TaskList)),
     ?assertEqual(split, Task#task.task_type),
     ?assertEqual(JobId, Task#task.job_id),
     % Testing if input file was created
     % If assertion fails, check the path.
-    % This test is ugly, as it depends on hard-coded path and will fail 
-    % for the rest.
-    %% TODO refactor this piece
+    % input.file should exist in storage before test
     ProgramFile = Root ++ "tmp/" ++ JobIdString ++ "/input/data.dat",
     chronicler:info(ProgramFile),
     ?assertEqual(ok, file:rename(ProgramFile, ProgramFile)),

1be3537b1f61334160660d299660fa8dfa9e242a
Vasilij Savin <vasilij.savin@gmail.com>
2009-10-15 12:34:51 +0000

    updated booter script.
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@311 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- lib/common/src/booter.erl
+++ lib/common/src/booter.erl
@@ -12,15 +12,20 @@
 %% erl -sname server -pa common/ebin -pa ecg/ebin -pa logger/ebin -pa master/ebin -run booter init_master
 
 %% Start slave node
-%% erl -sname client -pa common/ebin -pa ecg/ebin -pa logger/ebin -pa master/ebin/ -pa slave/ebin -run booter init_slave
+%% erl -pa common/ebin -pa ecg/ebin -pa logger/ebin -pa slave/ebin -run booter init_slave -name 'vj@130.238.15.217' -setcookie supercookie 
 
 init_master() ->
     application:start(chronicler),
     application:start(ecg),
     application:start(common),
     application:start(master),
-    db:create_tables().
+    db:create_tables(),
+    nodes(),
+    global:registered_names().
+
 init_slave() ->
     application:start(chronicler),
     application:start(common),
-    application:start(slave).
+    application:start(slave),
+    nodes(),
+    global:registered_names().

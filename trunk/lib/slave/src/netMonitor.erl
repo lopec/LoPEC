5f1d00ba6dd08eb4d460d9c403c8704ad193edb1
Fredrik Andersson <sedrik@consbox.se>
2009-10-27 11:05:00 +0000

    fixed path to ifconfig to work on system that dosen't allow ifconfig to be runned by users
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@435 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- trunk/lib/slave/src/netMonitor.erl
+++ trunk/lib/slave/src/netMonitor.erl
@@ -27,15 +27,15 @@
 get_net_stats() ->
     case os:cmd("uname") -- "\n" of
         "Darwin" ->
-	    A = lists:nth(2, string:tokens(os:cmd("netstat -I en0 -b"), "\n")),
-	    B = string:tokens(A, " "),
-	    {Up, Down} = {list_to_integer(lists:nth(10, B)),
-			  list_to_integer(lists:nth(7, B))};
+            A = lists:nth(2, string:tokens(os:cmd("netstat -I en0 -b"), "\n")),
+            B = string:tokens(A, " "),
+            {Up, Down} = {list_to_integer(lists:nth(10, B)),
+                list_to_integer(lists:nth(7, B))};
         "Linux" ->
-	    A = string:tokens(os:cmd("ifconfig eth0 | grep bytes"), ":"),
-	    Up = list_to_integer(hd(string:tokens(lists:nth(2, A), " "))),
-	    Down = list_to_integer(hd(string:tokens(lists:nth(3, A), " ")));
+            A = string:tokens(os:cmd("/sbin/ifconfig eth0 | grep bytes"), ":"),
+            Up = list_to_integer(hd(string:tokens(lists:nth(2, A), " "))),
+            Down = list_to_integer(hd(string:tokens(lists:nth(3, A), " ")));
         _ ->
-	    {Up, Down} = {0, 0}
+            {Up, Down} = {0, 0}
     end,
     {Up, Down}.

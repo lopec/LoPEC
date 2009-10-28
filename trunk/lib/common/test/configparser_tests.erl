8e4aef75808b4fd5c25438ce9d6c9082b422b85d
Niclas Axelsson <niclas@burbas.se>
2009-10-28 12:36:53 +0000

    Fixed test for configparser
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@489 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- trunk/lib/common/test/configparser_tests.erl
+++ trunk/lib/common/test/configparser_tests.erl
@@ -5,19 +5,27 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(configparser_tests).
-
 -include_lib("eunit/include/eunit.hrl").
-
 
 configparser_test_() ->
     {setup,
-     fun configparser:start_link/0,
-     fun (_) -> configparser:stop() end,
-     fun (_) ->
-             {inparallel,
-              [?_assertEqual({error, enoent},
-                             configparser:read_config("ysnftf, HEHE", key)),
-               ?_assertEqual({ok, "/storage/nfs/me"},
-                             configparser:read_config("src/testconfig.conf",
-                                                      nfs_mount))]}
-     end}.
+    fun() ->
+        {ok} end,
+     fun(_) ->   
+        {ok} end,   
+        
+     fun () ->
+         {inorder,
+            [
+                ?_assertEqual({error, enoent},
+                        configparser:read_config("ysnftf, HEHE", key)),
+                ?_assertEqual({ok, "/storage/nfs/me"},
+                        configparser:read_config("src/testconfig.conf", nfs_mount)),
+                ?_assertEqual({ok, "jansson"}, 
+                        configparser:parse("Apan", [{"Sven", "Kartofell"}, {"Apan", "jansson"}])),
+                ?_assertEqual({error, not_found}, 
+                        configparser:parse("Hitman Sedrik", [{"Apan", "jansson"}, {"Peter", "grävling"}])
+            ]
+          }
+    end
+    }.

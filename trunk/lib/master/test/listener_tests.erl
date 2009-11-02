3222c0290dbf7a78b9ed78c25997a8f458a77846
Niclas Axelsson <niclas@burbas.se>
2009-11-02 14:21:01 +0000

    Now the testcase for listener works
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@583 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- trunk/lib/master/test/listener_tests.erl
+++ trunk/lib/master/test/listener_tests.erl
@@ -11,80 +11,36 @@
 -include("../include/db.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
-init_test() ->
-    application:start(chronicler),
-    % chronicler:set_logging_level(all),
-    application:start(ecg),
-    application:start(common),
-    db:start_link(test),
-    examiner:start_link(),
-    listener:start_link(),
-    dispatcher:start_link().
-
-end_per_test_case(JobId) ->
-    db:remove_job(JobId).
-
-remove_jobs(Jobs) ->
-    [db:remove_job(JobId) || {ok, JobId} <- tl(Jobs)].
-
-job_name_test_() ->
+listener_test_() ->
     {setup,
-     fun () -> {ok, Root} =
-                   configparser:read_config("/etc/clusterbusters.conf", cluster_root),
-               [Root,
-                listener:add_job(raytracer, mapreduce, owner, 0, Root ++ "ray256", ray256),
-                listener:add_job(raytracer, mapreduce, owner, 0, Root ++ "ray256")
-               ]
+     fun () -> 
+        application:start(chronicler),
+        chronicler:set_logging_level(all),
+        application:start(ecg),
+        application:start(common),
+        db:start_link(test),
+        listener:start_link(),
+        dispatcher:start_link(),
+        {ok, Root} = configparser:read_config("/etc/clusterbusters.conf", cluster_root),
+        {ok, JobId} = listener:add_job(raytracer, mapreduce, owner, 0, Root++"lol.txt"),
+        {ok, JobId2} = listener:add_job(raytracer, mapreduce, owner2, 1, Root++"lol.txt", "ApanJansson"),
+        {JobId, JobId2}
      end,
-     fun (Jobs) -> remove_jobs(Jobs) end,
-     fun ([Root, {ok, Named}, {ok, Anonymous}]) ->
+     fun (_) ->  
+         application:stop(chronicler),
+         application:stop(ecg),
+         application:stop(common),
+         db:stop() 
+     end,
+     fun ({JobId, JobId2}) ->
              {inorder,
-              [?_assertEqual(anonymous, listener:get_job_name(Anonymous)),
-               ?_assertEqual({name, ray256}, listener:get_job_name(Named)),
-               ?_assertMatch({error, _},
-                             listener:add_job(raytracer, mapreduce, owner,
-                                              0, Root ++ "ray256", ray256)),
-               ?_assertEqual(ok, listener:remove_job_name(Named)),
-               ?_assertEqual(anonymous, listener:get_job_name(Named))                             
+              [
+                ?_assertEqual(anonymous, listener:get_job_name(JobId)),
+                ?_assertEqual({name, "ApanJansson"}, listener:get_job_name(JobId2)),
+                ?_assertEqual(ok, listener:pause_job(JobId)),
+                ?_assertEqual(anonymous, listener:get_job_name(123)), %% This is a non-existing job
+                ?_assertEqual(ok, listener:resume_job(JobId))
               ]
              }
      end
     }.
-
-job_creation_test() ->
-    chronicler:debug("Local processes: ~p", [erlang:registered()]),
-    chronicler:debug("Global processes: ~p", [global:registered_names()]),
-    %testing if job is created properly
-    
-    {ok, Root} = 
-        configparser:read_config("/etc/clusterbusters.conf", cluster_root),
-    
-    InputFile = Root ++ "ray256",
-    {ok, JobId} = listener:add_job(raytracer, mapreduce, owner, 0, InputFile),
-    Job = db:get_job(JobId),
-    chronicler:info(Job),
-    ?assertEqual(JobId, Job#job.job_id),
-    ?assertEqual(raytracer, Job#job.program_name),
-    
-    %Testing if task was properly created
-    JobIdString = integer_to_list(JobId),
-    TaskList = db:list(split_free),
-    ?assertMatch([_A], TaskList),
-    Task = db:get_task(hd(TaskList)),
-    ?assertEqual(split, Task#task.type),
-    ?assertEqual(JobId, Task#task.job_id),
-    % Testing if input file was created
-    % If assertion fails, check the path.
-    % input.file should exist in storage before test
-    ProgramFile = Root ++ "tmp/" ++ JobIdString ++ "/input/data.dat",
-    chronicler:info(ProgramFile),
-    %%TODO fix default test input file.
-    ?assertEqual(ok, file:rename(ProgramFile, ProgramFile)),
-    end_per_test_case(JobId).
-
-stop_test()->
-    application:stop(chronicler),
-    application:stop(ecg),
-    application:stop(common),
-    examiner:stop(),
-    db:stop().

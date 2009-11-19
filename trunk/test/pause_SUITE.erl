624f40408d9052c19106eb2d7e6c0abc905f7498
Fredrik Andersson <sedrik@consbox.se>
2009-11-19 11:59:37 +0000

    Added a pause_resume userstory common_test
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@670 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

---
+++ trunk/test/pause_SUITE.erl
@@ -0,0 +1,166 @@
+-module(pause_SUITE).
+-compile(export_all).
+-include_lib("common_test/include/ct.hrl").
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% common test callbacks %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+all() ->
+    [pause_resume_test].
+
+init_per_suite(Config) ->
+    % Args argument dosent seem to work :(
+    {ok, Master} = slave:start(localhost, master),
+        %, "+W w -pa ../,,/test -pa ../../lib/*/ebin"),
+    {ok, Slave} = slave:start(localhost, slave),
+        %, "+W w -pa ../,,/test -pa ../../lib/*/ebin"),
+
+    spawn(Master, code, add_path, ["../../test/"]),
+    spawn(Master, code, load_file, [pause_SUITE]),
+
+    spawn(Slave, code, add_path, ["../../test/"]),
+    spawn(Slave, code, load_file, [pause_SUITE]),
+
+    [{master, Master}, {slave, Slave} | Config].
+
+end_per_suite(Config) ->
+    {master, Master} = lists:keyfind(master, 1, Config),
+    {slave, Slave} = lists:keyfind(slave, 1, Config),
+
+    ok = slave:stop(Master),
+    ok = slave:stop(Slave),
+    ok.
+
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+
+%%%%%%%%%%%%%%%%
+%% test cases %%
+%%%%%%%%%%%%%%%%
+
+pause_resume_test(Config) ->
+    {master, Master} = lists:keyfind(master, 1, Config),
+    {slave, Slave} = lists:keyfind(slave, 1, Config),
+
+    MasterControll = fun(F) ->
+            receive
+                {request, {returnPid, From}} ->
+                    From ! {reply, self()},
+                    F(F);
+                {request, {start, From}} ->
+                    code:add_path("../../lib/common/ebin"),
+                    ok = application:start(common),
+                    code:add_path("../../lib/chronicler/ebin"),
+                    ok = application:start(chronicler),
+                    code:add_path("../../lib/ecg/ebin"),
+                    ok = application:start(ecg),
+                    code:add_path("../../lib/master/ebin"),
+                    ok = application:start(master),
+
+                    db:create_tables(ram_copies),
+
+                    chronicler:set_logging_level([user_info, error]),
+                    ok = chronicler:info(masterStarted),
+                    From ! {reply, ok},
+                    F(F);
+                {request, {add_jobs, From}} ->
+                    {ok, Pid1} = listener:add_job(wordcount, mapreduce,
+                        test, 1, "/storage/test/lol.txt"),
+                    {ok, Pid2} = listener:add_job(wordcount, mapreduce,
+                        test, 1, "/storage/test/lol.txt"),
+                    chronicler:user_info("Job1: ~p~n"
+                                         "Job2: ~p", [Pid1, Pid2]),
+                    From ! {reply, {Pid1, Pid2}},
+                    F(F);
+                {request, {pause, Job, From}} ->
+                    ok = listener:pause_job(Job),
+                    From ! {reply, ok},
+                    F(F);
+                {request, {resume, Job, From}} ->
+                    ok = listener:resume_job(Job),
+                    From ! {reply, ok},
+                    F(F)
+            end
+    end,
+
+    SlaveControll = fun(F) ->
+            receive
+                {request, {returnPid, From}} ->
+                    From ! {reply, self()},
+                    F(F);
+                {request, {start, From}} ->
+                    code:add_path("../../lib/common/ebin"),
+                    ok = application:start(common),
+                    code:add_path("../../lib/chronicler/ebin"),
+                    ok = application:start(chronicler),
+                    chronicler:set_logging_level([]),
+                    code:add_path("../../lib/slave/ebin"),
+                    ok = application:start(slave),
+
+                    From ! {reply, ok},
+                    F(F)
+            end
+    end,
+
+    M = spawn(Master, fun() -> MasterControll(MasterControll) end),
+    S = spawn(Slave, fun() -> SlaveControll(SlaveControll) end),
+
+    M ! {request, {returnPid, self()}},
+    receive
+        {reply, M} ->
+            ok
+    end,
+
+    S ! {request, {returnPid, self()}},
+    receive
+        {reply, S} ->
+            ok
+    end,
+
+    M ! {request, {start, self()}},
+    receive
+        {reply, ok} ->
+            ok
+    end,
+
+    S ! {request, {start, self()}},
+    receive
+        {reply, ok} ->
+            ok
+    end,
+
+    M ! {request, {add_jobs, self()}},
+    receive
+        {reply, {JobId1, JobId2}} ->
+                {JobId1, JobId2}
+    end,
+
+    %Job1 shold now have started, lets pause it.
+    M ! {request, {pause, JobId1, self()}},
+    receive
+        {reply, ok} ->
+                ok
+    end,
+
+    timer:sleep(10000),
+
+    %Job2 shold now be done, lets resume Job1
+    M ! {request, {resume, JobId1, self()}},
+    receive
+        {reply, ok} ->
+                ok
+    end,
+
+    timer:sleep(10000),
+
+    %lets diff the outcome and be happy if diff returns []
+    DiffCmd = lists:concat(["diff "
+        "/storage/test/results/", JobId1, "/word_count "
+        "/storage/test/results/", JobId2, "/word_count"]),
+    [] = os:cmd(DiffCmd),
+
+    ok.

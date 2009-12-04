647ce5a29b066e0e5975330419f8461661f58b92
Fredrik Andersson <sedrik@consbox.se>
2009-12-04 13:20:19 +0000

    moved catchalls.erl inside of common/src since it was messing with the tests (common_test didnt like it's location
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@845 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

---
+++ trunk/lib/common/src/catchalls.erl
@@ -0,0 +1,79 @@
+-module(catchalls).
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Logs and discards unexpected messages.
+%%
+%% @spec handle_call(Msg, From, State) ->  {noreply, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(Msg, From, State) ->
+    chronicler:warning("~w:Received unexpected handle_call call.~n"
+                       "Message: ~p~n"
+                       "From: ~p~n",
+                       [?MODULE, Msg, From]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Logs and discards unexpected messages.
+%%
+%% @spec handle_cast(Msg, State) ->  {noreply, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(Msg, State) ->
+    chronicler:warning("~w:Received unexpected handle_cast call.~n"
+                       "Message: ~p~n",
+                       [?MODULE, Msg]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Logs and discards unexpected messages.
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} 
+%% @end
+%%--------------------------------------------------------------------
+handle_info(Info, State) -> 
+    chronicler:warning("~w:Received unexpected handle_info call.~n"
+                       "Info: ~p~n",
+                       [?MODULE, Info]),
+    {noreply, State}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%% Logs and discards unexpected messages.
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(OldVsn, State, Extra) -> 
+    chronicler:warning("~w:Received unexpected code_change call.~n"
+                       "Old version: ~p~n"
+                       "Extra: ~p~n",
+                       [?MODULE, OldVsn, Extra]),
+    {ok, State}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%% Logs and discards unexpected messages.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(Reason, _State) -> 
+    chronicler:debug("~w:Received terminate call.~n"
+                     "Reason: ~p~n",
+                     [?MODULE, Reason]),
+    ok.

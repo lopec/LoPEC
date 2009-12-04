2bf765ad77f979ef5aa602a361840cc4500b20c2
Fredrik Andersson <sedrik@consbox.se>
2009-12-04 13:20:18 +0000

    small fixes to the test, it's broken right now though :(
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@844 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- trunk/lib/chronicler/test/chronicler_SUITE.erl
+++ trunk/lib/chronicler/test/chronicler_SUITE.erl
@@ -34,21 +34,22 @@ end_per_suite(_Config) ->
 % optional, can do function level setup for all functions,
 % or for individual functions by matching on TestCase.
 init_per_testcase(_TestCase, Config) ->
-    application:start(common),
-    application:start(chronicler),
-    {ok, File} = file:open(error_logger:logfile(filename), read),
+    ok = application:start(common),
+    ok = application:start(chronicler),
+    Filename = error_logger:logfile(filename),
+    {ok, File} = file:open(Filename, read),
     [{filePointer, File} | Config].
 
 % optional, can do function level tear down for all functions,
 % or for individual functions by matching on TestCase.
 end_per_testcase(_TestCase, [{filePointer, File}]) -> % do custom per suite cleanup here
-    file:close(File),
-    application:stop(chronicler),
-    application:stop(common),
+    ok = file:close(File),
+    ok = application:stop(chronicler),
+    ok = application:stop(common),
     ok;
 end_per_testcase(_TestCase, _Config) ->
-    application:stop(chronicler),
-    application:stop(common),
+    ok = application:stop(chronicler),
+    ok = application:stop(common),
     ok.
 
 %%%%%%%%%%%%%%%%
@@ -63,8 +64,9 @@ testing_log(File, LoggingLevel, LevelString, Msg, Arg) ->
     %"=INFO REPORT=" ++ _ = io:get_line(File, ""),
     %"Chronicler application started\n" = io:get_line(File, ""),
     "\n" = io:get_line(File, ""),
-    lists:prefix(LevelString, io:get_line(File, "")),
-    lists:prefix(lists:flatten(io_lib:format(Msg, Arg)), io:get_line(File, "")).
+    true = lists:prefix(LevelString, io:get_line(File, "")),
+    true = lists:prefix(lists:flatten(io_lib:format(Msg, Arg)), io:get_line(File, "")),
+    ok.
 
 info_log_test([{filePointer, File}]) ->
     testing_log(File, info, "=INFO REPORT=", "This is a info ~p", [test]);

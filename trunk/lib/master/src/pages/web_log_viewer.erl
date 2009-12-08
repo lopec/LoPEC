7f44ac80c26500d950c3e864a2f6891e61e51cb5
Fredrik Andersson <sedrik@consbox.se>
2009-12-08 10:35:29 +0000

    nice log output
    git-svn-id: file:///Users/burbas/Documents/Programmering/lopec/lopec@871 7a0f0faa-8d40-49f0-87d8-be473b26c0a8

--- trunk/lib/master/src/pages/web_log_viewer.erl
+++ trunk/lib/master/src/pages/web_log_viewer.erl
@@ -14,23 +14,80 @@ footer() ->
 get_info() ->
     common_web:get_info().
 
-% Creates the menu.
 menu() ->
     common_web:menu().
 
-% Creates the submenu.        
 submenu() ->
     common_web:submenu().
 
 body() ->
     [
-    #rounded_panel {
-        color=gray,
-        body=[
-            #h2{text="LogView"},
-            #h3{text=wf:f("~p~n", [main_chronicler:get_everything()])}
-        ]
-            }
-        ].
+        #rounded_panel {
+            color=gray,
+            body=[
+                #h2{text="LogView"},
+                #button { text="Filter view", postback=filter_settings },
+                #p{},
+                print_log_table()
+            ]
+        }
+    ].
+
+event(filter_settings) ->
+    %% Flash message
+    wf:flash([
+            #h2{text="Filter view"},
+            #p{},
+
+            #label{id=log_type, text="Type"},
+            #checkbox{text="Information", checked=true},
+            #checkbox{text="Warnings", checked=true},
+            #checkbox{text="Errors", checked=true},
+            #p{},
+
+            #label{id=log_node, text="Client node"},
+            #dropdown{options= [
+                    #option {text="node1@clusterbusters" ,value=1},
+                    #option {text="node2@clusterbusters" ,value=1},
+                    #option {text="node3@clusterbusters" ,value=1}
+                ]},
+            #p{},
+
+            #button { id=continueButton, text="Filter", postback=filter_logs}
+        ]);
 
 event(_) -> ok.
+
+print_log_table() ->
+    LogMessages = main_chronicler:get_all_logs(),
+    #table { id=logTable ,class="jobtable", rows=[
+            #tablerow { cells=[
+                    #tableheader { text="Type" },
+                    #tableheader { text="Username" },
+                    #tableheader { text="Node" },
+                    #tableheader { text="Time" }
+                ]}|parse_log_messages(LogMessages)
+        ]}.
+
+%[
+%    [lopec_warning,nisse,slave@localhost,
+%        {{2009,12,8},{11,20,51}},
+%        "burbas är en ko"],
+%    [lopec_warning,nisse2,slave@localhost,
+%        {{2009,12,8},{11,22,11}},
+%        "burbas är en ko2"]
+%         ]
+
+parse_log_messages([]) -> [];
+parse_log_messages([[Type, User, Node, Time, Message]|T]) ->
+    [#tablerow { cells=[
+                #tablecell { text=Type },
+                #tablecell { text=User },
+                #tablecell { text=Node },
+                #tablecell { text=parse_time(Time) }
+            ]},
+        #tablerow{ cells=[ #tablecell{ text=Message, colspan=4}]}
+        |parse_log_messages(T)].
+
+parse_time({{Year,Month,Day},{Hour,Minute,Second}}) ->
+    lists:concat([Day,"/",Month,"-",Year," ",Hour,":",Minute,".",Second]).
